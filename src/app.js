f71cd60a1198e64f6c3492958cb3ffae58e92444
Maxime Daffis <malkovitch@gmail.com>
2018-03-20 17:31:00 +0100

    setup redux & router through an exemple

--- src/app.js
+++ src/app.js
@@ -1,21 +1,40 @@
-import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./app.css";
+// import React, { Component } from "react";
+// import logo from "./logo.svg";
+// import "./app.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <h1 className="App-title">Welcome to React</h1>
+//         </header>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+
+import React from "react";
+import { Route, Link } from "react-router-dom";
+import Home from "./containers/home";
+import About from "./containers/about";
+
+const App = () => (
+  <div>
+    <header>
+      <Link to="/">Home</Link>
+      <Link to="/about-us">About</Link>
+    </header>
+
+    <main>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about-us" component={About} />
+    </main>
+  </div>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,30 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./app";
-import registerServiceWorker from "./registerServiceWorker";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import "./index.css";
+// import App from "./app";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// ReactDOM.render(<App />, document.getElementById("root"));
+
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import { ConnectedRouter } from "react-router-redux";
+import store, { history } from "./store";
+import registerServiceWorker from "./registerServiceWorker";
+import App from "./app";
+
+// import './index.css'
+
+const target = document.getElementById("root");
+
+render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <div>
+        <App />
+      </div>
+    </ConnectedRouter>
+  </Provider>,
+  target
+);
 registerServiceWorker();
